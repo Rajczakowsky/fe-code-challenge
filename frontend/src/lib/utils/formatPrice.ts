8563d40ee7a13ca78a2a540d7260fd66088446ac
Mariusz Rajczakowski <mariusz.rsky@gmail.com>
2025-03-11 14:07:29 +0000

    MR013: Add function to format price

--- frontend/src/lib/utils/formatPrice.ts
+++ frontend/src/lib/utils/formatPrice.ts
@@ -1,11 +1,26 @@
-export function formatPrice(num: number): string {
+/**
+ * Formats a number as a price in USD.
+ *
+ * @param {number} num - The number to format.
+ * @param {number} [minimumIntegerDigits=1] - The minimum number of integer digits.
+ * @param {number} [minimumFractionDigits=0] - The minimum number of fraction digits.
+ * @param {number} [maximumFractionDigits=1] - The maximum number of fraction digits.
+ * @returns {string} The formatted price string.
+ */
+
+export function formatPrice(
+  num: number,
+  minimumIntegerDigits: number = 1,
+  minimumFractionDigits: number = 0,
+  maximumFractionDigits: number = 1
+): string {
   const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
-    minimumIntegerDigits: 1,
-    maximumFractionDigits: 0,
-    minimumFractionDigits: 0,
+    minimumIntegerDigits,
+    maximumFractionDigits,
+    minimumFractionDigits,
     currency: 'USD'
   });
 
-  return formatter.format(Math.floor(num));
+  return formatter.format(num);
 }
